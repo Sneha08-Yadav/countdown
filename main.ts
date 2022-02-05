9893cc38ba2d095745cd21b16760c82817d5ab27
Sneha08-Yadav <87853092+Sneha08-Yadav@users.noreply.github.com>
2022-02-05 20:41:06 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+for (let index = 0; index <= 9; index++) {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    basic.showNumber(10 - index)
+}
+music.playTone(392, music.beat(BeatFraction.Double))
+basic.showString("Happy new year")
